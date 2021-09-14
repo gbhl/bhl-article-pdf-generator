ebab2c0fb6fd3669bf67bd79501aeb8c4c728951
Joel Richard <github@joelrichard.com>
2021-09-14 14:24:32 -0400

    New stats script

---
+++ statistics.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Counts the files, pages, bytes in the output directory
+#
+#
+#
+
+SUM=0
+COUNT=0
+TOTAL=$(ls -1 ./output | wc -l)
+SIZE=$(du -skh ./output | cut -f 1 -d ' ')
+
+for FILE in ./output/*.pdf; do
+   LINES=$(strings < $FILE | sed -n 's|.*/Count -\{0,1\}\([0-9]\{1,\}\).*|\1|p' | sort -rn | head -n 1)
+   SUM=$(($SUM+$LINES))
+   COUNT=$(($COUNT+1))
+   echo -en "\rReading $COUNT/$TOTAL..."
+done
+echo
+echo "Number of Files: $COUNT"
+echo "Total Lines: $SUM"
+echo "Total Size: $SIZE"
