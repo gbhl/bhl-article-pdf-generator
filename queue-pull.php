bc98cb6b65d237f5655c0a91fbc1053d14aebaa9
Joel Richard <github@joelrichard.com>
2021-12-01 16:31:28 -0500

    Mo' memory\!

--- queue-pull.php
+++ queue-pull.php
@@ -3,6 +3,7 @@ namespace QueueWatcher;
 require_once __DIR__ . '/vendor/autoload.php';
 require_once './packages/bhl/pdfgenerator/src/PDFGenerator.php';
 require_once './packages/bhl/pdfgenerator/src/ForceJustify.php';
+ini_set('memory_limit','1024M');
 
 use PDODb;
 use Noodlehaus\Config;
