6b363976e3d296a8047523aaeb8dac7e5a02f9ec
Joel Richard <github@joelrichard.com>
2021-12-13 17:23:51 -0500

    Fixed multi-process handling

--- coordinator.php
+++ coordinator.php
@@ -1,6 +1,9 @@
+#!/opt/rh/rh-php73/root/usr/bin/php
 <?php
 $max = 4;
-$child_script = "sleeper.php";
+$child_script = "queue-pull.php";
+
+
 $command =  PHP_BINARY." {$child_script} > /dev/null 2>&1 &";
 
 function count_children($child_script) {
@@ -8,15 +11,17 @@ function count_children($child_script) {
 	return (int)trim($ret);
 }
 
+$fh = fopen('log/coordinator.log', 'a');
 while (true) {
 	$count = count_children($child_script);
-	print "$count < $max\n";
 	if (file_exists('cancel.txt')) {
 		break;
 	}
-	if ($count < $max) {
-		print "Parent spawning child...\n";
+	if ($count <= $max) {
+		$dt = new DateTime();
+		$dt = $dt->format('Y-m-d H:i:s');
+		fwrite($fh, "[{$dt}] Parent spawning child...\n");
 		`$command`;
 	}
-	sleep(1);
+	sleep(10);
 }
