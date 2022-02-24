8be3128fce87c2cc155032e33ca3ca56de169ce3
Joel Richard <github@joelrichard.com>
2022-02-24 08:30:54 -0500

    incremental improvements

--- pdf-generator.php
+++ pdf-generator.php
@@ -7,24 +7,41 @@
 // pushing through the BHL PDF message queue.
 // ******************************************
 
-$max = 4;
+$max = 8;
 $child_script = "queue-pull.php";
+
+if (isset($argv[1])) {
+	$max = $argv[1];
+}
 
 function count_children($child_script) {
 	$ret =  `ps awx | fgrep {$child_script} | fgrep -v grep | wc -l`;
 	return (int)trim($ret);
 }
 
-$fh = fopen('log/coordinator.log', 'a');
+function log_message($m) {
+	$fh = fopen('log/coordinator.log', 'a');
+	$dt = new DateTime();
+	$dt = $dt->format('Y-m-d H:i:s');
+	fwrite($fh, "[{$dt}] {$m}\n");
+	fclose($fh);
+}
+
+function clear_cache() {
+	log_message("Clearing cache...");
+	`find ./cache -mtime +2 -exec rm -fr {} \; > /dev/null 2>&1`;
+	`find ./cache -mtime +2 -exec rm -fr {} \; > /dev/null 2>&1`;
+	`find ./cache -mtime +2 -exec rm -fr {} \; > /dev/null 2>&1`;
+	`find ./cache -mtime +2 -exec rm -fr {} \; > /dev/null 2>&1`;
+	`find ./cache -mtime +2 -exec rm -fr {} \; > /dev/null 2>&1`;
+	log_message("Finished clearing cache.");
+}
+
 while (true) {
 	$count = count_children($child_script);
-	if (file_exists('cancel.txt')) {
-		break;
-	}
-	if ($count <= $max) {
-		$dt = new DateTime();
-		$dt = $dt->format('Y-m-d H:i:s');
-		fwrite($fh, "[{$dt}] Parent spawning child...\n");
+	if ($count < $max) {
+		log_message("Parent spawning child...");
+		clear_cache();
 		$command =  PHP_BINARY." {$child_script} > log/queue-pull-".time().".txt 2>&1 &";
 		`$command`;
 	}
